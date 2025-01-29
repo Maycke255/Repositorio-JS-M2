64ecf8499b2e2e4d0252f78a752c5316a8a9d594
Maycke255 <reydsonk@gmail.com>
2025-01-28 21:35:32 -0300

    Enviando arquivos.

--- Exercicio-1/Parte-3/js/multiplicacao.js
+++ Exercicio-1/Parte-3/js/multiplicacao.js
@@ -4,10 +4,12 @@ let numbers = []
 alert(`Você precisa digitar pelo menos 3 números.\n
     E então o programa vai multiplicar os 3 numeros ate chegar em 10`)
 
-let option = parseInt(prompt(`Quais números você deseja multiplicar?`));
+let option = prompt(`Quais números você deseja multiplicar?`);
+numbers.push(option)
 
-for (let i = 0 ; i <= 10 ; i++){
-    let operation = i * numbers
-    alert(`Multiplicação dos numeros: ${numbers}\n
-        ${operation}`)
+let operation = ``
+
+for (let i = 0 ; i < numbers.length ; i++){
+    operation += `Seu número:  ${(i+1)}  ${numbers[i]}\n`
+    alert(`Multiplicação dos numeros: ${operation}\n`)
 }
