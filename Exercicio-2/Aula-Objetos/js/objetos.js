a23a78629ee1f555666a6ad60990634f894c34c9
Maycke255 <reydsonk@gmail.com>
2025-01-30 16:58:53 -0300

    Enviando arquivos atualizados.

---
+++ Exercicio-2/Aula-Objetos/js/objetos.js
@@ -0,0 +1,15 @@
+const eu = {
+    nome: "Maycke",
+    idade: 21,
+    cidade: "Taiobeiras-MG",
+    profissão: "Programador",
+    cursos: [`UX/UI design a prova de balas`,
+        `HTML`,
+        `CSS`,
+        `CSS Moderno`,
+        `Javascript Básico`, 
+        `Superior em analise e desenvolvimento de sistemas.`]
+}
+
+console.log(eu)
+alert(eu.cursos[0])
