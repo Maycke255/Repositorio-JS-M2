757be352666f0e61ceb425a334b8d684a51be0da
Maycke255 <reydsonk@gmail.com>
2025-02-06 17:21:05 -0300

    Enviando algumas alterações na aula sobre funções

--- Exercicio-3/Aula-Funcoes/js/script.js
+++ Exercicio-3/Aula-Funcoes/js/script.js
@@ -4,7 +4,7 @@ function calcularMedia(a, b) {
 }
 
 const resultadoMedia = calcularMedia(7, 2)
-console.log(resultadoMedia)
+// console.log(resultadoMedia)
 
 function objeto (nome = "Digite um nome", preco = "não definido", quantidade = 0){
     const produto = {
@@ -17,4 +17,40 @@ function objeto (nome = "Digite um nome", preco = "não definido", quantidade = 
 
 const venda = objeto("Placa de video 5090 TI", 1050, 3)
 
-console.log(venda)
+// console.log(venda)
+
+
+
+function soma (numero1, numero2){
+    return numero1 + numero2
+}
+
+const resultado = soma(12, 12)
+
+// console.log(resultado)
+
+function areaRetangular (base, altura){
+    return base * altura
+}
+
+// console.log(areaRetangular(12, 4))
+
+function maiorIdade(valor) {
+    if (valor >= 18) {
+        console.log("Você de maior.")
+        return "Você tem mais de 18 anos."
+        let inacessivel = console.log("Valor nuca lido.")
+    } else {
+        console.log("Você e de menor.")
+        return "Você ainda tem menor de 18 anos."
+    }
+}
+
+// console.log(maiorIdade(34))
+// console.log(maiorIdade(15))
+
+function multiplicacaoDe2 (youNumber){
+    console.log(`Resultado: ${youNumber * 2}`)
+}
+
+multiplicacaoDe2(30)
